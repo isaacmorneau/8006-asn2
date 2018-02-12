0921bca3c9c0f90836c62ab2f351b84e284dfdc4
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-12 11:29:34 +0000

    fixed firewall cleanup

--- firewall.sh
+++ firewall.sh
@@ -9,6 +9,10 @@ INT='enp3s2'
 IP='192.168.1.1'
 IP_BASE='192.168.1.0'
 MASK='255.255.255.0'
+NAMESERVER='''
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+'''
 
 echo "Enabling interface $INT"
 ifconfig $INT up
@@ -17,3 +21,7 @@ ifconfig $INT $IP netmask $MASK
 echo "1" >/proc/sys/net/ipv4/ip_forward
 route add -net 192.168.0.0 netmask $MASK gw 192.168.0.100
 route add -net $IP_BASE/24 gw $IP
+
+cp /etc/resolv.conf resolvBACKUP
+echo "$NAMESERVER" > /etc/resolv.conf
+
