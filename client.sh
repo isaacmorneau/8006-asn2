dfd5c8b5fd3b78670a2a07574e82d941d8b94440
John Agapeyev <jagapeyev@gmail.com>
2018-02-12 10:29:22 -0800

    Added setting of default route

--- client.sh
+++ client.sh
@@ -7,9 +7,12 @@ fi
 
 INT='enp3s2'
 IP='192.168.1.5'
+DEST='192.168.1.6'
 MASK='255.255.255.0'
 
 echo "Enabling interface $INT"
 ifconfig $INT up
 echo "Setting IP $IP/$MASK"
 ifconfig $INT $IP netmask $MASK
+echo "Setting default gateway to $DEST"
+ip route add default via $DEST dev $INT
