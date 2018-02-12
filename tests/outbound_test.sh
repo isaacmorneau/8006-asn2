d43f3a5e3a04c126a09356f516d462f69a1cdfab
John Agapeyev <jagapeyev@gmail.com>
2018-02-12 15:26:42 -0800

    Added filter scanning, wow

--- tests/outbound_test.sh
+++ tests/outbound_test.sh
@@ -1,6 +1,6 @@
 #Outgoing fragments
 echo "Fragment test"
-if [ "`hping3 -f -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -f -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
@@ -8,7 +8,7 @@ fi
 
 #DNS packets
 echo "DNS test"
-if [ "`hping3 -p 53 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -p 53 -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
@@ -16,7 +16,7 @@ fi
 
 #SYN/FIN packets
 echo "SYN/FIN test"
-if [ "`hping3 -SF -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -SF -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
@@ -24,59 +24,23 @@ fi
 
 #Telnet packets
 echo "Telnet test"
-if [ "`hping3 -p 23 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -p 23 -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
 fi
 
 #TCP ports 32768-32775
-echo "Port 32767 test"
-if [ "`hping3 -p 32767 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
-    echo "Test passed"
-else
-    echo "Test FAILED"
-fi
-echo "Port 32768 test"
-if [ "`hping3 -p 32768 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
-    echo "Test passed"
-else
-    echo "Test FAILED"
-fi
-echo "Port 32770 test"
-if [ "`hping3 -p 32770 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
-    echo "Test passed"
-else
-    echo "Test FAILED"
-fi
-echo "Port 32775 test"
-if [ "`hping3 -p 32775 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
-    echo "Test passed"
-else
-    echo "Test FAILED"
-fi
-echo "Port 32776 test"
-if [ "`hping3 -p 32776 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+echo "Ports 32768-32775 test"
+if [ "`nmap -p 32768-32775 8.8.8.8 | grep filtered | wc -l`" == "8" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
 fi
 
 #TCP ports 137-139
-echo "Port 137 test"
-if [ "`hping3 -p 137 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
-    echo "Test passed"
-else
-    echo "Test FAILED"
-fi
-echo "Port 138 test"
-if [ "`hping3 -p 138 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
-    echo "Test passed"
-else
-    echo "Test FAILED"
-fi
-echo "Port 139 test"
-if [ "`hping3 -p 139 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+echo "Port 137-139 test"
+if [ "`nmap -p 137-139 8.8.8.8 | grep [sr]ed | wc -l`" == "3" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
@@ -84,14 +48,14 @@ fi
 
 #TCP ports 111, and 515
 echo "Port 111 test"
-if [ "`hping3 -p 111 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -p 111 -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
 fi
 
 echo "Port 515 test"
-if [ "`hping3 -p 515 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -p 515 -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
@@ -99,7 +63,14 @@ fi
 
 #Send SYN/ACK without SYN
 echo "Orphan SYN/ACK test"
-if [ "`hping3 -SA -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+if [ "`hping3 -SA -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+#TCP Christmas
+echo "Chirstmas test"
+if [ "`hping3 -FSRPAUXY -c 3 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
     echo "Test passed"
 else
     echo "Test FAILED"
