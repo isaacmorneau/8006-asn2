d8685f4e117f3c64e5f8b4f47fa60b3d7591289b
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-12 14:56:28 +0000

    fixed grepable output

--- tests/inbound_test.sh
+++ tests/inbound_test.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 read -p "Enter firewall IP: " IP
 read -p "Enter host IP: " HIP
-BLOCKED_PORTS=`nmap -Pn -oG -p 32768-32775,137-139,111,515 $IP`
+BLOCKED_PORTS=`nmap -Pn -oG - -p 32768-32775,137-139,111,515 $IP`
 SYNFIN=`hping3 -S -F -c 1 $IP`
 TEL=`hping3 -S -p 23 -c 1 $IP`
 HOST=`hping3 -S -p 80 -c 1 $HIP`
