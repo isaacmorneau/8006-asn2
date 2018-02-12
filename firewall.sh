81da693a873d646f7a71591f7331328f26c22165
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-12 10:28:02 +0000

    added setup for firewall

---
+++ firewall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+    echo "Requesting Root" 1>&2
+    sudo ${0}
+    exit 0
+fi
+
+INT='enp3s2'
+IP='192.168.1.1'
+MASK='255.255.255.0'
+
+echo "Enabling interface $INT"
+ifconfig $INT up
+echo "Setting IP $IP/$MASK"
+ifconfig $INT $IP netmask $MASK
