0a942c5679565962ab0cd94ab0c4d032e3009f72
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-12 14:51:03 +0000

    added further tests

--- tests/inbound_test.sh
+++ tests/inbound_test.sh
@@ -4,5 +4,10 @@ if [[ $EUID -ne 0 ]]; then
     sudo ${0}
     exit 0
 fi
-read -p "Enter IP to test: " IP
-nmap -Pr -oG -p 32768-32775,137-139,111,515 $IP
+read -p "Enter firewall IP: " IP
+read -p "Enter host IP: " HIP
+BLOCKED_PORTS=`nmap -Pn -oG -p 32768-32775,137-139,111,515 $IP`
+SYNFIN=`hping3 -S -F -c 1 $IP`
+TEL=`hping3 -S -p 23 -c 1 $IP`
+HOST=`hping3 -S -p 80 -c 1 $HIP`
+
