2247084d0285b097b71171816837dd8083b0b6ab
John Agapeyev <jagapeyev@gmail.com>
2018-02-12 10:54:19 -0800

    Fixed client config

--- client.sh
+++ client.sh
@@ -5,14 +5,24 @@ if [[ $EUID -ne 0 ]]; then
     exit 0
 fi
 
+GLOBAL='eno1'
 INT='enp3s2'
 IP='192.168.1.5'
 MASK='255.255.255.0'
 GATE='192.168.1.1'
 
+NAMESERVER='''
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+'''
+
+echo "Disabling interface $GLOBAL"
+ifconfig $GLOBAL down
 echo "Enabling interface $INT"
-ifconfig $INT up
-echo "Setting IP $IP/$MASK"
-ifconfig $INT $IP netmask $MASK
+ifconfig p3p1 $IP up
 echo "Setting Gateway $GATE"
-ip route add default via $GATE dev $INT
+route add default gw $GATE
+
+cp /etc/resolv.conf resolvBACKUP
+echo $NAMESERVER > /etc/resolv.conf
+
