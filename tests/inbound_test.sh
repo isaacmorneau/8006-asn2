b6cfd0573defb444fcea3956bdbadecbcb2d6ff6
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-12 15:01:30 +0000

    added echos

--- tests/inbound_test.sh
+++ tests/inbound_test.sh
@@ -10,4 +10,11 @@ BLOCKED_PORTS=`nmap -Pn -oG - -p 32768-32775,137-139,111,515 $IP`
 SYNFIN=`hping3 -S -F -c 1 $IP`
 TEL=`hping3 -S -p 23 -c 1 $IP`
 HOST=`hping3 -S -p 80 -c 1 $HIP`
-
+echo "Blocked Ports"
+echo $BLOCKED_PORTS
+echo "Blocked SynFin"
+echo $SYNFIN
+echo "Blocked Telnet"
+echo $TEL
+echo "Blocked Internal"
+echo $HOST
