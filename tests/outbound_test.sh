fa7ea667f3704cde84160a08b0b126bbdbf0b1ac
John Agapeyev <jagapeyev@gmail.com>
2018-02-12 14:59:00 -0800

    Added testing format to tests

--- tests/outbound_test.sh
+++ tests/outbound_test.sh
@@ -1,30 +1,106 @@
 #Outgoing fragments
-hping3 -f -c 10 8.8.8.8
+echo "Fragment test"
+if [ "`hping3 -f -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #DNS packets
-hping3 -p 53 -c 10 8.8.8.8
+echo "DNS test"
+if [ "`hping3 -p 53 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #SYN/FIN packets
-hping3 -SF -c 10 8.8.8.8
+echo "SYN/FIN test"
+if [ "`hping3 SF -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #Telnet packets
-hping3 -p 23 -c 10 8.8.8.8
+echo "Telnet test"
+if [ "`hping3 -p 23 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #TCP ports 32768-32775
-hping3 -p 32767 -c 10 8.8.8.8
-hping3 -p 32768 -c 10 8.8.8.8
-hping3 -p 32770 -c 10 8.8.8.8
-hping3 -p 32775 -c 10 8.8.8.8
-hping3 -p 32776 -c 10 8.8.8.8
+echo "Port 32767 test"
+if [ "`hping3 -p 32767 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+echo "Port 32768 test"
+if [ "`hping3 -p 32768 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+echo "Port 32770 test"
+if [ "`hping3 32770 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+echo "Port 32775 test"
+if [ "`hping3 32775 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+echo "Port 32776 test"
+if [ "`hping3 32776 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #TCP ports 137-139
-hping3 -p 137 -c 10 8.8.8.8
-hping3 -p 138 -c 10 8.8.8.8
-hping3 -p 139 -c 10 8.8.8.8
+echo "Port 137 test"
+if [ "`hping3 -p 137 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+echo "Port 138 test"
+if [ "`hping3 -p 138 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+echo "Port 139 test"
+if [ "`hping3 -p 139 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #TCP ports 111, and 515
-hping3 -p 111 -c 10 8.8.8.8
-hping3 -p 515 -c 10 8.8.8.8
+echo "Port 111 test"
+if [ "`hping3 -p 111 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
+
+echo "Port 515 test"
+if [ "`hping3 -p 515 -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
 
 #Send SYN/ACK without SYN
-hping3 -SA -c 10 8.8.8.8
+echo "Orphan SYN/ACK test"
+if [ "`hping3 -SA -c 10 8.8.8.8 2>&1 > /dev/null | grep -o -i 100%`" == "100%" ]; then
+    echo "Test passed"
+else
+    echo "Test FAILED"
+fi
