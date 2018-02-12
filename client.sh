906917bba6307a2033d74fd22362d24b5e708563
Isaac Morneau <isaacmorneau@gmail.com>
2018-02-12 10:30:17 +0000

    added client gateway

--- client.sh
+++ client.sh
@@ -8,8 +8,11 @@ fi
 INT='enp3s2'
 IP='192.168.1.5'
 MASK='255.255.255.0'
+GATE='192.168.1.1'
 
 echo "Enabling interface $INT"
 ifconfig $INT up
 echo "Setting IP $IP/$MASK"
 ifconfig $INT $IP netmask $MASK
+echo "Setting Gateway $GATE"
+ip route add default via $GATE dev $INT
